e040b150efdab4fe90440fd20802216198de53b1
borderlesste <borderlesstechno7@gmail.com>
2025-08-17 19:57:19 -0300

    Update index.js
    server url

--- server/src/index.js
+++ server/src/index.js
@@ -67,6 +67,7 @@ const corsOptions = {
     const allowedOrigins = isProduction
       ? ['https://borderlesstechno.com', 'https://www.borderlesstechno.com']
       : [
+        'https://saas-backend-33g1.onrender.com',
           'http://localhost:5173', 
           'http://localhost:4000', 
           'http://127.0.0.1:5173',
